dd277b7358c5e69b4e6db20a865693e10361b626
Lachlan Paul <lachlanpaul03@gmail.com>
2024-04-30 19:46:51 +0930

    openai

--- app.py
+++ app.py
@@ -5,14 +5,27 @@
 """
 import os
 
-import mockgpt
-
 import whisper
+from dotenv import load_dotenv
+from openai import OpenAI
 from flask import Flask, request, abort
 
 APP = Flask(__name__)
 WHISPER_MODEL = whisper.load_model("base")
 PASSCODE = "p$T9wQz2a#R8fL!sE6hGn5vXyY3jU7iKo0bC1xZ4qJmO"
+
+load_dotenv()
+OPENAI_KEY = os.getenv("OPENAI_KEY")
+
+client = OpenAI(api_key=OPENAI_KEY)
+
+assistant = client.beta.assistants.create(
+  name="Pepper Robot",
+  instructions="You are the Pepper Robot produced by SoftBank Robotics. You are polite, bubbly, and ready to help in any way you can",
+  model="gpt-3.5-turbo",
+)
+
+new_thread = client.beta.threads.create()
 
 if not os.path.exists("uploads"):
     os.makedirs("uploads")
@@ -21,7 +34,8 @@ if not os.path.exists("uploads"):
 @APP.route("/upload", methods=["POST"])
 def upload_file():
     files = request.files["audio_file"]
-    files.save(f"uploads/{files.filename}")
+    file_path = f"uploads/{files.filename}"
+    files.save(file_path)
     client_passcode = request.headers.get("Passcode")
 
     # Checks to make sure file is only a wav.
@@ -34,11 +48,28 @@ def upload_file():
     if client_passcode != PASSCODE:
         abort(401)
 
-    result = WHISPER_MODEL.transcribe(files.filename)
-    # Delete the file to save space, also because I might get shit if I kept recordings of people talking to a robot.
-    os.remove(f"uploads/{files.filename}")
+    # Transcribe the saved file instead of the file object
+    with open(file_path, 'rb') as audio_file:
+        whisper_result = WHISPER_MODEL.transcribe(audio_file)
+    os.remove(file_path)
 
-    return mockgpt.response_to_text(result["text"])
+    message = client.beta.threads.messages.create(
+        thread_id=new_thread.id,
+        role="user",
+        content=whisper_result
+    )
+
+    run = client.beta.threads.runs.create_and_poll(
+        thread_id=new_thread.id,
+        assistant_id=assistant.id
+    )
+
+    while True:
+        if run.status == "completed":
+            messages = client.beta.threads.messages.list(
+                thread_id=new_thread.id
+            )
+            print(messages[-1])
 
 
 if __name__ == "__main__":
--- mockgpt.py
+++
@@ -1,2 +0,0 @@
-def response_to_text(input):
-    return f"Wow, you sure just said '{input}'"
